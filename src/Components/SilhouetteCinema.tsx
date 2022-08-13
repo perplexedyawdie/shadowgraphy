da5d1dfacbe5bfa2f768f38cf43af012d790018b
perplexedyawdie <sunkwenga@gmail.com>
2022-08-13 16:25:31 -0500

    added webcam

--- src/Components/SilhouetteCinema.tsx
+++ src/Components/SilhouetteCinema.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Webcam from "react-webcam";
+import { useWindowSize } from 'react-use';
+
 
 function SilhouetteCinema() {
-  return (
-    <div>SilhouetteCinema</div>
-  )
+    const { width, height } = useWindowSize();
+
+    return (
+        <div className="h-screen w-full">
+            <Webcam
+                audio={false}
+                width={width}
+                height={height}
+                screenshotFormat="image/jpeg" />
+        </div>
+    )
 }
 
 export default SilhouetteCinema
